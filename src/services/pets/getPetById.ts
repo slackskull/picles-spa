b6b1ade2513940d0c0c3d893b6a55529b667fa02
LucasForti <lukas_discovery@hotmail.com>
2024-04-22 22:23:20 -0300

    Feat: Adicionando botao whats

---
+++ src/services/pets/getPetById.ts
@@ -0,0 +1,14 @@
+import { IPet } from "../../interfaces/pet"
+import httpClient from "../api/httpClient"
+
+
+export async function getPetById(id: string): Promise<IPet> {
+    try{
+        const response = await httpClient (`/pet/${id}`)
+        return response.data
+    }catch (error) {
+        console.error ('Erro ao buscar Pet por id', error)
+        throw error
+    }
+}
+
